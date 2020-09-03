ee0b17b4b187d1605d611f052996298b9494dae5
Sam0fc <sammyhutton@gmail.com>
2020-09-03 13:48:34 -0700

    New java file.

---
+++ change.java
@@ -0,0 +1,13 @@
+public class Change
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, world!");
+  }
+  public static int AuxNumWays(int cents)
+  {
+    if (cents==0){
+      return 1; 
+    }
+  }
+}
